9b90d005582cf60175eeacd74aee523ace9f4e09
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-11-06 15:31:20 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	int a, b, c;
+	cin >> a >> b >> c;
+	// edge case: if n is equal to 1, then there's no need to travel
+	if (n == 1) {
+		cout << 0 << '\n';
+		return 0;
+	}
+	// view the image in this repository for better understanding
+	// otherwise, decrement n since Winnie-the-Pooh is already in Rabbit's house
+	// afterwards, choose the starting point, min(a, b)
+	// compare the minimum distance between going back to the previous house or
+	// going to the another house
+	n -= 1;
+	int start = min(a, b);
+	cout << start + (min(start, c) * (n - 1)) << '\n';
+	return 0;
+}
